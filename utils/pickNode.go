405773855a65504b9fc29b1bf239aa422ee03ddc
sodiqscript111 <simidusodiqt@gmail.com>
2026-08-23 12:28:45 +0100

    Add 5-tuple hashing and PickNode load balancing logic

---
+++ utils/pickNode.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"encoding/binary"
+	"hash/crc32"
+)
+
+func (t FiveTuple) Hash() uint32 {
+	h := crc32.NewIEEE()
+
+	if t.SrcIP != nil {
+		h.Write(t.SrcIP)
+	}
+	if t.DstIP != nil {
+		h.Write(t.DstIP)
+	}
+
+	var buf [5]byte
+	binary.BigEndian.PutUint16(buf[0:2], t.SrcPort)
+	binary.BigEndian.PutUint16(buf[2:4], t.DstPort)
+	buf[4] = byte(t.Protocol)
+
+	h.Write(buf[:])
+
+	return h.Sum32()
+}
+
+func PickNode(tuple FiveTuple, nodes []string) string {
+	if len(nodes) == 0 {
+		return ""
+	}
+
+	hashVal := tuple.Hash()
+	selectedIdx := hashVal % uint32(len(nodes))
+	return nodes[selectedIdx]
+}
